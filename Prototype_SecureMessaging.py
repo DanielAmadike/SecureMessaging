3a0fde0839a1306e995fedcfa96a6a31acefe927
Daniel Amadike <amadykedaniel@gmail.com>
2026-01-04 22:56:56 +0000

    Initial commit

---
+++ Prototype_SecureMessaging.py
@@ -0,0 +1,18 @@
+
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+# AES ENCRYPTION FUNCTION
+
+def encrypt_aes(plaintext):
+    """
+    Encrypts a plaintext message using AES
+    and measures the encryption time.
+    """
+    # Generate a random 128-bit (16-byte) AES key
+    key = get_random_bytes(16)
+
+    # Create a new AES cipher object in GCM mode
+    cipher = AES.new(key, AES.MODE_GCM)
+
